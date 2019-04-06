a4798ada9a75df5f2b10123241ece5fad6709137
motians <motians@comcast.net>
2019-04-06 14:18:06 -0700

    login added to main

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import os
 import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
-from model import Donation, Donor
+from model import Donation, Donor, User
+from passlib.hash import pbkdf2_sha256
+
 import peewee
 
 app = Flask(__name__)
+app.secret_key = b'\x9d\xb1u\x08%\xe0\xd0p\x9bEL\xf8JC\xa3\xf4J(hAh\xa4\xcdw\x12S*,u\xec\xb8\xb8'
+# app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 
 @app.route('/')
@@ -25,6 +29,9 @@ def all():
 def create():
     """ Function for creating donation """
 
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
     if request.method == 'POST':
         try:
             donor_lookup = Donor.select().where(Donor.name == request.form['name']).get()
@@ -36,6 +43,23 @@ def create():
             return render_template('create.jinja2', error='Donor does not exist.')
 
     return render_template('create.jinja2')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    """ User login """
+
+    if request.method == 'POST':
+        try:
+            got_user = User.select().where(User.name == request.form['name']).get()
+
+            if got_user and pbkdf2_sha256.verify(request.form['password'], got_user.password):
+                session['username'] = request.form['name']
+                return redirect(url_for('create'))
+        except peewee.DoesNotExist:
+            return render_template('login.jinja2', error='Login failure.')
+
+    return render_template('login.jinja2')
 
 
 if __name__ == "__main__":
