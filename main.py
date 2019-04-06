fe45980dec2d896b426b0f023a4c51869e6367cb
motians <motians@comcast.net>
2019-04-06 12:29:26 -0700

    Function for donor create

--- main.py
+++ main.py
@@ -2,28 +2,39 @@ import os
 import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
-
-from model import Donation, Donor, db
+from model import Donation, Donor
+import peewee
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
+    """ Function to display list of all donations """
+
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
 
+
 @app.route('/create/', methods=['GET', 'POST'])
 def create():
+    """ Function for creating donation """
+
     if request.method == 'POST':
-        donor = Donor.select().where(Donor.name == request.form['name']).get()
-        if donor:
-            Donation.create(name=request.form['name'], value=request.form['value'])
-            return render_template('donations.jinja2')
-        return render_template('create.jinja2', error='Donor does not exist.')
+        try:
+            donor_lookup = Donor.select().where(Donor.name == request.form['name']).get()
+
+            if donor_lookup:
+                Donation.create(donor=donor_lookup, value=request.form['value'])
+                return redirect(url_for('all'))
+        except peewee.DoesNotExist:
+            return render_template('create.jinja2', error='Donor does not exist.')
+
     return render_template('create.jinja2')
 
 
